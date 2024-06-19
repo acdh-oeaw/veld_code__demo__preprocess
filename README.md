79ccb046580f1d498634d6f861e3c9d66c2c0afa
SteffRhes <steffrhes@pm.me>
2024-06-19 12:32:51 +0100

    update with default standalone logic

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# veld_code__demo__wikipedia_loader
+# veld_code__demo__preprocess
+
+### what is it?
+
+This is a demo veld code repo. It contains code that is mainly intended to be integrated into a veld
+chain, but can also be run on its own, by defaulting to the `data` folder.
+
+### what does it do?
+
+It simply downloads the bible, and prepares it for word2vec training (removes newlines, makes all
+text lower-case, splits the data into sentences, and saves it as a file with each line being a
+sentence).
+
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,10 @@
+import os
 import nltk
 import requests
 from nltk.tokenize import sent_tokenize
+
+
+OUT_FILE = "/veld/output/" + os.getenv("out_file")
 
 
 # load sentence splitting function
@@ -15,7 +19,7 @@ content = sent_tokenize(content)
 
 
 # write to file, one sentence per line
-with open("/veld/output/bible.txt", "w") as f:
+with open(OUT_FILE, "w") as f:
     for line_nr, sentence in enumerate(content):
         f.write(sentence + "\n")
         # from line 29810 onwards, there is only gutenberg metadata left, so break the loop
